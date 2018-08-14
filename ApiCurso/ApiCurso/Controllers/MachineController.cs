41e8bb4b47b44524241f4ab216d1dfb2016171bd
mayaramerlin <mayaramerlin@hotmail.com>
2018-08-13 22:59:17 -0300

    novo controller

---
+++ ApiCurso/ApiCurso/Controllers/MachineController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCurso.Controllers
+{
+    [Route("api/[controller]")]
+    public class MachineController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetMachine()
+        {
+            return Ok(new { name = Environment.MachineName});
+        }
+    }
+}
